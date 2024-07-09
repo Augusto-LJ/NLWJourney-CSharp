7e9a9ad08c4e3ebfdf92fafca3e5f26ebae097f0
Augusto Lima Jardim <augustolimajardim@gmail.com>
2024-07-09 17:32:51 -0300

    Cria o use case RegisterTripUseCase e valida a requisição

---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Journey.Communication.Requests;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public void Execute(RequestRegisterTripJson request)
+        {
+            Validate(request);
+        }
+
+        private void Validate(RequestRegisterTripJson request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Name))
+            {
+                throw new ArgumentException("Nome não é pode ser vazio.");
+            }
+
+            if(request.StartDate.Date < DateTime.UtcNow.Date)
+            {
+                throw new ArgumentException("A viagem não pode ser registrada para uma data passada.");
+            }
+
+            if (request.EndDate.Date < request.StartDate.Date)
+            {
+                throw new ArgumentException("A data final não pode ser menor que a data inicial.");
+            }
+        }
+    }
+}
