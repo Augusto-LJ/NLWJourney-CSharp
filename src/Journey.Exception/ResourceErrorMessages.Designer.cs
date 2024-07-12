d6affc97d5642cc11572075c3cb37618ab99f6dd
Augusto Lima Jardim <augustolimajardim@gmail.com>
2024-07-11 22:23:02 -0300

    Implementa endpoints para criar, completar e deletar atividades

--- src/Journey.Exception/ResourceErrorMessages.Designer.cs
+++ src/Journey.Exception/ResourceErrorMessages.Designer.cs
@@ -61,6 +61,24 @@ namespace Journey.Exception {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Activity not found..
+        /// </summary>
+        public static string ACTIVITY_NOT_FOUND {
+            get {
+                return ResourceManager.GetString("ACTIVITY_NOT_FOUND", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The date you select for the activity is not within your travel period..
+        /// </summary>
+        public static string DATE_NOT_WITHIN_TRAVEL_PERIOD {
+            get {
+                return ResourceManager.GetString("DATE_NOT_WITHIN_TRAVEL_PERIOD", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The end date of the trip must be later than the start date..
         /// </summary>
         public static string END_DATE_TRIP_MUST_BE_LATER_THAN_START_DATE {
